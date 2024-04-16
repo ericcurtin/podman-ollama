5f2de860466b0141f682a9f26edc65a7e65711db
Eric Curtin <ecurtin@redhat.com>
2024-04-16 09:20:03 +0100

    Nvidia changes
    There's a toolkit for containers
    
    Signed-off-by: Eric Curtin <ecurtin@redhat.com>

--- install.sh
+++ install.sh
@@ -2,14 +2,19 @@
 
 set -e -o pipefail
 
+available() {
+  command -v $1 >/dev/null
+}
+
 install_dnf() {
   if sed "s/\s/\n/g" /proc/cmdline | grep -q -m1 "^ostree="; then
+    RPM_OSTREE="true"
     if rpm-ostree install podman 2>&1 | grep -v "already provided"; then
       echo 'Reboot to complete podman install.'
     fi
-  elif command -v dnf > /dev/null; then
+  elif available dnf; then
     dnf -y install podman
-  elif command -v yum > /dev/null; then
+  elif available yum; then
     yum -y install podman
   fi
 }
@@ -23,22 +28,39 @@ cleanup() {
   rm -rf "$TMP" &
 }
 
+check_gpu() {
+  if available "lspci" && lspci -d '10de:' | grep -q 'NVIDIA'; then
+    NVIDIA="true"
+  fi
+
+  if available "lshw" && $SUDO lshw -c display -numeric | grep -q 'vendor: .* \[10DE\]'; then
+    NVIDIA="true"
+  fi
+}
+
 if [ "$EUID" -ne 0 ]; then
   echo "Please run as root"
   exit 0
 fi
 
-# This is a downstream version of the ollama install script, until podman
-# related patches get reviewed
-URL="raw.githubusercontent.com/ericcurtin/podman-ollama/s/ollama-install.sh"
-curl -fsSL "https://$URL" | OLLAMA_CONTAINER_MANAGER="podman" sh
-
 . /etc/os-release
 
+RPM_OSTREE="false"
 case $ID in
-  centos|rhel|rocky|fedora|amzn) install_dnf ;;
+  centos|rhel|autosd|rocky|fedora|fedora-asahi-remix|amzn) install_dnf ;;
   debian|ubuntu) install_apt ;;
 esac
+
+NVIDIA="false"
+check_gpu
+
+if ! $NVIDIA && ! $RPM_OSTREE; then
+  # This is a downstream version of the ollama install script, until podman
+  # related patches get reviewed:
+  # https://github.com/ollama/ollama/pulls/ericcurtin
+  URL="raw.githubusercontent.com/ericcurtin/podman-ollama/s/ollama-install.sh"
+  curl -fsSL "https://$URL" | OLLAMA_CONTAINER_MANAGER="podman" sh
+fi
 
 for BINDIR in /usr/local/bin /usr/bin /bin; do
   echo $PATH | grep -q $BINDIR && break || continue
